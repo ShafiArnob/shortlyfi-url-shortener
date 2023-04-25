b7d3a51342d1ca47f536d1459543498bea53e4ae
ShafiArnob <shafiarnob@gmail.com>
2023-04-25 21:56:52 +0600

    UI: Stats page ui change

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -18,22 +18,36 @@ const Stats = () => {
 
   }, [id])
   return (
-    <div className="h-[100vh] flex flex-col justify-center items-center">
+    <div className="h-[100vh]  flex flex-col justify-center items-center">
 
-      <div>
-        <div className="flex space-x-4">
-          <p>Long URL: {url.longUrl}</p>
-          <button className="bg-red-500 px-3 py-1">Copy</button>
+      <div className="space-y-3 bg-[#1b1c30] p-8 w-1/3 rounded-2xl z-20 opacity-85 border-t-2 border-l-2 border-[#7b54e9] border-opacity-30">
+        <div className="flex justify-between">
+          <h1 className="text-3xl text-[#ad8fff] font-bold pb-3">Shortlyfi</h1>
+
+          <div className="ml-1 flex items-center space-x-4">
+            <p className="font-bold text-xl text-gray-400">Visited</p>
+            <span className="font-bold text-lg text-gray-300 bg-[#252740] px-2 py-1 rounded-lg">{url.visited}</span>
+          </div>
+
+        </div> 
+        
+        <div className="space-y-1">
+          <p className="ml-1 text-lg text-gray-400 font-semibold">Long URL</p>
+          <div className="flex rounded-2xl bg-[#252740] overflow-hidden">
+            <input type="text" value={url.longUrl} className="p-2 py-4 mx-2 w-full focus:outline-none  text-gray-300 bg-[#252740]" disabled/>
+            <button className="m-2 rounded-xl bg-[#7b54e9] hover:bg-[#6143b6] px-3 py-1">Copy</button>
+          </div>
         </div>
 
-        <div className="flex space-x-4">
-          <p>Short URL: {url.shortUrl}</p>
-          <button className="bg-red-500 px-3 py-1">Copy</button>
+        <div className="space-y-1">
+          <p className="ml-1 text-lg text-gray-400 font-semibold">Short URL</p>
+          <div className="flex rounded-2xl bg-[#252740] overflow-hidden">
+            <input type="text" value={url.shortUrl} className="p-2 py-4 mx-2 w-full focus:outline-none  text-gray-300 bg-[#252740] " disabled/>
+            <button className="m-2 rounded-xl bg-[#7b54e9] hover:bg-[#6143b6] px-3 py-1">Copy</button>
+          </div>
         </div>
 
-        <div>
-          <p>Visited: {url.visited}</p>
-        </div>
+        
       </div>
 
     </div>
